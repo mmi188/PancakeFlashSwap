1c454f4d9fe72d3d6665f7d61a7b28388542eeeb
mmi188 <micmih3@gmail.com>
2023-05-20 17:15:04 +0200

    imports

--- contracts/FlashSwap.sol
+++ contracts/FlashSwap.sol
@@ -3,3 +3,12 @@ pragma solidity >=0.6.6;
 
 import "hardhat/console.sol";
 
+// Uniswap interface and library imports
+import "./libraries/UniswapV2Library.sol";
+import "./libraries/SafeERC20.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IERC20.sol";
+
